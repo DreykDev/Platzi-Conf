6da4a384d905ce4176749bffaf939a572d66a26e
David Valencia <codingdavid9@outlook.com>
2021-05-26 17:08:24 -0500

    props - eventos

--- src/pages/BadgeNew.jsx
+++ src/pages/BadgeNew.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './styles/BadgeNew.css'
 import Navbar from '../components/Navbar.jsx'
 import Badge from '../components/Badge'
+import BadgeForm from '../components/BadgeForm'
 
 import header from '../images/badge-header.svg'
 
@@ -9,6 +10,7 @@ class BadgeNew extends React.Component {
   render() {
     return (
       <div>
+
         <Navbar/>
 
         <div className="BadgeNew__hero">
@@ -17,10 +19,16 @@ class BadgeNew extends React.Component {
 
         <div className="container">
           <div className="rox" />
-            <div className="col">
+            <div className="col-6">
               <Badge firtsName="David" lastName="Valencia" />
             </div>
+
+            <div className="col-6">
+              <BadgeForm />
+            </div>
+
         </div>
+
       </div>
     );
   }
